a61aa4acbdea87af9dc505922aef4d08bc336fb8
唐道源 <tangdaoyuan@baijiahulian.com>
2019-09-19 09:55:09 +0800

    test

--- src/routes/IndexPage.tsx
+++ src/routes/IndexPage.tsx
@@ -12,7 +12,7 @@ class IndexPage extends React.Component {
         <ul className="list">
           <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
           <li>
-            <Button type="danger">Click</Button>
+            <Button type="link">Click</Button>
           </li>
         </ul>
       </div>
